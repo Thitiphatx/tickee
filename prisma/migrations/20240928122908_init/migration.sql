bb49e15ad887121875a87eb226dbb9c719bd4829
Ratanon090 <Ratanon1@gmail.com>
2024-09-29 18:13:48 +0700

    b4 brief

--- prisma/migrations/20240928122908_init/migration.sql
+++ prisma/migrations/20240928122908_init/migration.sql
@@ -1,50 +1,19 @@
 -- CreateTable
-CREATE TABLE "accounts" (
-    "id" TEXT NOT NULL,
-    "user_id" TEXT NOT NULL,
-    "type" TEXT NOT NULL,
-    "provider" TEXT NOT NULL,
-    "provider_account_id" TEXT NOT NULL,
-    "refresh_token" TEXT,
-    "access_token" TEXT,
-    "expires_at" INTEGER,
-    "token_type" TEXT,
-    "scope" TEXT,
-    "id_token" TEXT,
-    "session_state" TEXT,
-
-    CONSTRAINT "accounts_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "sessions" (
-    "id" TEXT NOT NULL,
-    "session_token" TEXT NOT NULL,
-    "user_id" TEXT NOT NULL,
-    "expires" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "sessions_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "users" (
     "id" TEXT NOT NULL,
     "name" TEXT,
     "email" TEXT,
     "password" TEXT,
+    "image" TEXT,
     "role" TEXT NOT NULL DEFAULT 'user',
     "idCard" TEXT,
     "mobile" TEXT,
-    "birthDate" TIMESTAMP(3),
+    "birthDate" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "provider" TEXT NOT NULL DEFAULT 'credentials',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "verificationtokens" (
-    "identifier" TEXT NOT NULL,
-    "token" TEXT NOT NULL,
-    "expires" TIMESTAMP(3) NOT NULL
 );
 
 -- CreateTable
@@ -110,7 +79,7 @@ CREATE TABLE "Promotion" (
     "pro_id" SERIAL NOT NULL,
     "seat_type_id" INTEGER NOT NULL,
     "pro_description" TEXT NOT NULL,
-    "pro_discount" DOUBLE PRECISION,
+    "pro_discount" DOUBLE PRECISION NOT NULL,
     "pro_start_date" TIMESTAMP(3) NOT NULL,
     "pro_last_date" TIMESTAMP(3) NOT NULL,
     "pro_type_id" INTEGER NOT NULL,
@@ -136,28 +105,13 @@ CREATE TABLE "Admin_Data" (
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "accounts_provider_provider_account_id_key" ON "accounts"("provider", "provider_account_id");
-
--- CreateIndex
-CREATE UNIQUE INDEX "sessions_session_token_key" ON "sessions"("session_token");
-
--- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
-
--- CreateIndex
-CREATE UNIQUE INDEX "verificationtokens_identifier_token_key" ON "verificationtokens"("identifier", "token");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Seat_Dispatch_seat_type_id_key" ON "Seat_Dispatch"("seat_type_id");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Promotion_seat_type_id_key" ON "Promotion"("seat_type_id");
-
--- AddForeignKey
-ALTER TABLE "accounts" ADD CONSTRAINT "accounts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "sessions" ADD CONSTRAINT "sessions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Receipt" ADD CONSTRAINT "Receipt_rec_customer_id_fkey" FOREIGN KEY ("rec_customer_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
