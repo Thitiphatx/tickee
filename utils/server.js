69e254422799513ca83369f4b8a3af2f2b6d977b
FrozeNxSouL <zenixzelon@hotmail.com>
2024-10-22 23:49:28 +0700

    server - testing cron task

--- utils/server.js
+++ utils/server.js
@@ -1,6 +1,5 @@
 import { CronJob } from 'cron';
-import { autoChangeReceiptStatus } from './function';
-import { getReceiptDate } from '@/app/admin/fetch';
+import { autoChangeReceiptStatus, getReceiptDate } from './function';
 
 var lastCalculation = null;
 
@@ -8,16 +7,19 @@ const graphTask = new CronJob('*/1 * * * *',async function () {
     let temp = await getReceiptDate();
     if (temp) {
         lastCalculation = temp
+        console.log("graphTask")
     }
 });
 graphTask.start();
 
 const changeReceiptStatusTask = new CronJob('*/1 * * * *',async function () {
     await autoChangeReceiptStatus()
+    console.log("changeRECstatus")
 });
 changeReceiptStatusTask.start();
 
 export function getLastCalculationResult() {
+    console.log("call data function",lastCalculation.data)
     return new Promise((resolve) => {
         resolve(lastCalculation);
     });
