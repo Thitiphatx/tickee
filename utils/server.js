639cfda81c30aa2e2e4803dfd24abca17575c29e
FrozeNxSouL <zenixzelon@hotmail.com>
2024-11-02 15:15:47 +0700

    cronjob

--- utils/server.js
+++ utils/server.js
@@ -1,27 +1,27 @@
-import { CronJob } from 'cron';
-import { autoChangeReceiptStatus, getReceiptDate } from './function';
+// import { CronJob } from 'cron';
+// import { autoChangeReceiptStatus, getReceiptDate } from './function';
 
-var lastCalculation = null;
+// var lastCalculation = null;
 
-const graphTask = new CronJob('*/1 * * * *',async function () {
-    let temp = await getReceiptDate();
-    if (temp) {
-        lastCalculation = temp
-        console.log("graphTask")
-    }
-});
-graphTask.start();
+// const graphTask = new CronJob('*/1 * * * *',async function () {
+//     let temp = await getReceiptDate();
+//     if (temp) {
+//         lastCalculation = temp
+//         console.log("graphTask")
+//     }
+// });
+// graphTask.start();
 
-const changeReceiptStatusTask = new CronJob('*/1 * * * *',async function () {
-    await autoChangeReceiptStatus()
-    console.log("changeRECstatus")
-});
-changeReceiptStatusTask.start();
+// const changeReceiptStatusTask = new CronJob('*/1 * * * *',async function () {
+//     await autoChangeReceiptStatus()
+//     console.log("changeRECstatus")
+// });
+// changeReceiptStatusTask.start();
 
-export function getLastCalculationResult() {
-    console.log("call data function",lastCalculation.data)
-    return new Promise((resolve) => {
-        resolve(lastCalculation);
-    });
-}
+// export function getLastCalculationResult() {
+//     console.log("call data function",lastCalculation.data)
+//     return new Promise((resolve) => {
+//         resolve(lastCalculation);
+//     });
+// }
 
