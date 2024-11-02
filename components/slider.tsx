0de3431f4d02f1f8bf5ff456a6d751fc101590e2
Thitiphatx <thitiphat6391@gmail.com>
2024-11-02 14:57:25 +0700

    fixed gallery

--- components/slider.tsx
+++ components/slider.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination, Navigation, Autoplay } from 'swiper/modules';
 import 'swiper/css';
@@ -7,45 +7,28 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
-const ImageSlider: React.FC = () => {
+export default function ImageSlider({ image_item }: { image_item: string[] }) {
 
-  const [images,setimages] = useState<string[]>([]);
-
-  useEffect (()=>{
-    const fecthbanner = async () => {
-      try{
-        const response = await fetch('/api/webConfigData');
-        if (!response.ok) {
-          throw new Error('Failed to fetch images');
-        }
-        const data = await response.json();
-        setimages(data.banner_images);
-      }
-      catch(error){
-        console.error('Error fetching banner images:', error);
-      }
+    if (image_item.length == 0) {
+        return (
+            <></>
+        )
     }
-    fecthbanner();
-    },[]);
-
-
-  return (
-    <Swiper
-      spaceBetween={10}
-      slidesPerView={1}
-      pagination={{ clickable: true }}
-      navigation
-      scrollbar={{ draggable: true }}
-      autoplay={{ delay: 3000 }}
-      modules={[Pagination, Navigation, Autoplay]}
-    >
-      {images.map((src, index) => (
-        <SwiperSlide key={index}>
-          <img src={src} alt={`Slide ${index + 1}`} style={{ width: '100%', height: 'auto' }} />
-        </SwiperSlide>
-      ))}
-    </Swiper>
-  );
+    return (
+        <Swiper
+            spaceBetween={10}
+            slidesPerView={1}
+            pagination={{ clickable: true }}
+            navigation
+            scrollbar={{ draggable: true }}
+            autoplay={{ delay: 3000 }}
+            modules={[Pagination, Navigation, Autoplay]}
+        >
+            {image_item.map((src, index) => (
+                <SwiperSlide key={index}>
+                    <img src={src} alt={`Slide ${index + 1}`} style={{ width: '100%', height: 'auto' }} />
+                </SwiperSlide>
+            ))}
+        </Swiper>
+    );
 };
-
-export default ImageSlider;
