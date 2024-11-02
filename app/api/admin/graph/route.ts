8a1549c5f7069fc9812bf74552c99e764f0b4ba9
FrozeNxSouL <zenixzelon@hotmail.com>
2024-11-02 15:56:38 +0700

    cronjob

--- app/api/admin/graph/route.ts
+++ app/api/admin/graph/route.ts
@@ -1,15 +1,15 @@
-// import { getLastCalculationResult } from '@/utils/server';
-// import { NextResponse } from 'next/server';
+import { getReceiptDate } from '@/utils/function';
+import { NextResponse } from 'next/server';
 
-// export async function GET() {
-//     try {
-//         const data = await getLastCalculationResult();
-//         if (data) {
-//             return NextResponse.json(data);
-//         } else {
-//             return NextResponse.json({ status: 404 , message: 'Return Data = Null' });
-//         }
-//     } catch (error) {
-//         return NextResponse.json({ status: 500, message: 'Internal Server Error' });
-//     }
-// }
+export async function GET() {
+    try {
+        const data = await getReceiptDate();
+        if (data) {
+            return NextResponse.json(data);
+        } else {
+            return NextResponse.json({ status: 404 , message: 'Return Data = Null' });
+        }
+    } catch (error) {
+        return NextResponse.json({ status: 500, message: 'Internal Server Error' });
+    }
+}
